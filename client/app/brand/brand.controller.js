a3ed84fad85f243f01451c853cdd528970a4f4a9
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 07:10:47 +0100

    Update brand.controller.js

--- client/app/brand/brand.controller.js
+++ client/app/brand/brand.controller.js
@@ -7,5 +7,6 @@ angular.module('materialShopApp')
       {field: 'brand', dataType: 'number', heading: 'ID'},
       {field: 'image', dataType:'image'},
       {field: 'active', dataType: 'boolean'}
+    
     ];
   });
