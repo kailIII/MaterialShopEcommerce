8092cf2cbdebdc0811b995917ec3f22f0016997e
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-16 14:13:22 +0100

    Update contact.controller.spec.js

--- client/app/contact/contact.controller.spec.js
+++ client/app/contact/contact.controller.spec.js
@@ -11,6 +11,7 @@ describe('Controller: ContactCtrl', function () {
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
+    
     scope = $rootScope.$new();
     ContactCtrl = $controller('ContactCtrl', {
       $scope: scope
