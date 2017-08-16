ab11d6af7e59d618affe31eca144735a49bf72c8
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-16 14:43:15 +0100

    Update customer.controller.spec.js

--- client/app/customer/customer.controller.spec.js
+++ client/app/customer/customer.controller.spec.js
@@ -14,6 +14,7 @@ describe('Controller: CustomerCtrl', function () {
   beforeEach(inject(function ($controller, $rootScope) {
     
     scope = $rootScope.$new();
+    
     CustomerCtrl = $controller('CustomerCtrl', {
       $scope: scope
     });
