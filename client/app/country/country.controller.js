e318cd8d5185697bf195b4eaaf725d8dba096086
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-16 14:19:47 +0100

    Update country.controller.js

--- client/app/country/country.controller.js
+++ client/app/country/country.controller.js
@@ -5,7 +5,8 @@ angular.module('materialShopApp')
   .controller('CountryCtrl', function ($scope) {
     
   $scope.options = [
-      {field: 'name'},
+      
+    {field: 'name'},
       {field: 'dial_code'},
       {field: 'code'},
       {field: 'active', dataType: 'boolean'}
