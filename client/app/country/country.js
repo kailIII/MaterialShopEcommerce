7299ac3a3a55134f66aa3823d5b072be6d1bb3ba
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-08-16 14:27:21 +0100

    Update country.js

--- client/app/country/country.js
+++ client/app/country/country.js
@@ -13,6 +13,7 @@ angular.module('materialShopApp')
     templateUrl: 'app/country/country.html',
         
     controller: 'CountryCtrl',
-        authenticate: 'manager'
+        
+    authenticate: 'manager'
       });
   });
